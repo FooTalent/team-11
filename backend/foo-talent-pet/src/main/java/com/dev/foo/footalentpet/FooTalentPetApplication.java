fdf971196ec8c81471f7a1c25f1ffac1b160d879
Nicolás Lemos <nico.lemos77@gmail.com>
2024-06-12 14:29:42 -0300

    first commit

---
+++ backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/FooTalentPetApplication.java
@@ -0,0 +1,13 @@
+package com.dev.foo.footalentpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooTalentPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooTalentPetApplication.class, args);
+    }
+
+}
---
+++ backend/foo-talent-pet/src/test/java/com/dev/foo/footalentpet/FooTalentPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.foo.footalentpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooTalentPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

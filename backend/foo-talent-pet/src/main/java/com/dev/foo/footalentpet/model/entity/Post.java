f2c0334d697f6eb1b69abb8bbef9b9391bf9b7a5
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-06-18 16:03:25 -0300

    build: post/entity, enum status and repostiroy

---
+++ backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/model/entity/Post.java
@@ -0,0 +1,85 @@
+package com.dev.foo.footalentpet.model.entity;
+
+import com.dev.foo.footalentpet.model.enums.PostStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    private Date date;
+
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public PostStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PostStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

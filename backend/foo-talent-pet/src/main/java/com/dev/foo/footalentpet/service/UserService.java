47d9b589f35a88666a88258a6377e32080ee4ee2
Nicolás Lemos <nico.lemos77@gmail.com>
2024-06-26 16:35:36 -0300

    User service interface and Implementation

--- backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/service/UserService.java
+++ backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/service/UserService.java
@@ -1,10 +1,12 @@
 package com.dev.foo.footalentpet.service;
 
 
+import com.dev.foo.footalentpet.model.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
-
-
+    List<User> getAllUsers();
 }

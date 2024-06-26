a44cc18ee1df7034d292f8ee82e760eb0d2c3db1
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-06-26 18:20:27 -0300

    build: swagger in post

--- backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/controller/PostController.java
+++ backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/controller/PostController.java
@@ -10,7 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@Tag(name = "Post", description = "Post operations")
 @RestController
 @RequestMapping("/post")
 public class PostController {
@@ -18,6 +23,10 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Operation(summary = "Create a new post", description = "Creates a new post and returns the post data", responses = {
+            @ApiResponse(responseCode = "201", description = "Successfully created post"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping
     public ResponseEntity<PostResponseDTO> create(@RequestBody PostRequestDTO postRequestDTO) {
         PostResponseDTO post = postService.create(postRequestDTO);

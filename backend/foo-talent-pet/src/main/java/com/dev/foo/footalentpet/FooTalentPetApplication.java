a85fa1634a52871e3b58933920864aa136f4a51b
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-06-29 23:06:47 -0300

    fix: swagger cong

--- backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/FooTalentPetApplication.java
+++ backend/foo-talent-pet/src/main/java/com/dev/foo/footalentpet/FooTalentPetApplication.java
@@ -1,10 +1,14 @@
 package com.dev.foo.footalentpet;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@OpenAPIDefinition(servers = {@Server(url = "/api", description = "Default Server URL")}, info = @Info(title = "Pet-Quest API", version = "1.0", description = "API for Pet-Quest"))
 public class FooTalentPetApplication {
 
     public static void main(String[] args) {

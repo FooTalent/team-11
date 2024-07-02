5b3f7c421727662d732350682e676214fe1d56c3
Juan Vacca <jvaccape@ibero.edu.co>
2024-07-02 18:05:30 -0500

    se incluye footer en otras vistas, se configura el root global, se inlcuye en assets los insumos visuales que se generan por parte de diseño

--- frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-mascotasperdidas',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './mascotasperdidas.component.html',
   styleUrl: './mascotasperdidas.component.css'
 })

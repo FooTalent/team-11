5b32cfc2182bba9f1a678363a282ae1a9e5093c3
Maicol bautista <pirlo1121s@gmail.com>
2024-07-03 19:54:24 -0500

    {+} create filtro component and boceto filtro

---
+++ frontend/foo-talent-pet/src/app/components/filtroperdidas/filtroperdidas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtroperdidas',
+  standalone: true,
+  imports: [],
+  templateUrl: './filtroperdidas.component.html',
+  styleUrl: './filtroperdidas.component.css'
+})
+export class FiltroperdidasComponent {
+
+}
--- frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
 
 @Component({
   selector: 'app-mascotasperdidas',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent],
   templateUrl: './mascotasperdidas.component.html',
   styleUrl: './mascotasperdidas.component.css'
 })

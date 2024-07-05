65a3d1e4a183ac609fc2fe6998ea1ebc9d7f5025
Daniel Romero <romeroblessjunior@gmail.com>
2024-07-05 11:35:00 -0500

    joind us v1

--- frontend/foo-talent-pet/src/app/components/home/home.component.ts
+++ frontend/foo-talent-pet/src/app/components/home/home.component.ts
@@ -6,20 +6,14 @@ import { PetQuestService } from '../../service/pet-quest.service';
 import { OnInit } from '@angular/core';
 import { LandingTestimonialComponent } from "../landing-testimonial/landing-testimonial.component";
 import { LandingAboutusComponent } from '../landing-aboutus/landing-aboutus.component';
-<<<<<<< Updated upstream
-import { LandingComofuncionaComponent } from '../landing-comofunciona/landing-comofunciona.component';
-=======
 import { LandingEncuentraComponent } from '../landing-encuentra/landing-encuentra.component';
->>>>>>> Stashed changes
+import { LandingUneteComponent } from '../landing-unete/landing-unete.component';
+import { LandingComofuncionaComponent } from "../landing-comofunciona/landing-comofunciona.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [NavbarComponent, FooterComponent, RouterLink,LandingTestimonialComponent,LandingAboutusComponent,LandingComofuncionaComponent],
-=======
-  imports: [NavbarComponent, FooterComponent, RouterLink,LandingTestimonialComponent,LandingAboutusComponent, LandingEncuentraComponent],
->>>>>>> Stashed changes
+  imports: [NavbarComponent, FooterComponent, RouterLink,LandingTestimonialComponent,LandingAboutusComponent, LandingEncuentraComponent,LandingComofuncionaComponent,LandingUneteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

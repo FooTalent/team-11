1c977690d88eeeb9045d9910429d020073c97aef
Maicol bautista <pirlo1121s@gmail.com>
2024-07-04 20:42:05 -0500

    {+} icon filter and colors filter

--- frontend/foo-talent-pet/src/app/components/mascotasadopcion/mascotasadopcion.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasadopcion/mascotasadopcion.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
 
 @Component({
   selector: 'app-mascotasadopcion',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent],
   templateUrl: './mascotasadopcion.component.html',
   styleUrl: './mascotasadopcion.component.css'
 })
--- frontend/foo-talent-pet/src/app/components/mascotasencontradas/mascotasencontradas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasencontradas/mascotasencontradas.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
 
 @Component({
   selector: 'app-mascotasencontradas',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent,FiltroperdidasComponent],
   templateUrl: './mascotasencontradas.component.html',
   styleUrl: './mascotasencontradas.component.css'
 })

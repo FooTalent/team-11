a02d114049708adfbd08500811262d1cc441c003
Juan Vacca <jvaccape@ibero.edu.co>
2024-07-05 13:01:55 -0500

    landing encuentra a tu mejor amigo en componente

--- frontend/foo-talent-pet/src/app/components/home/home.component.ts
+++ frontend/foo-talent-pet/src/app/components/home/home.component.ts
@@ -6,14 +6,24 @@ import { PetQuestService } from '../../service/pet-quest.service';
 import { OnInit } from '@angular/core';
 import { LandingTestimonialComponent } from "../landing-testimonial/landing-testimonial.component";
 import { LandingAboutusComponent } from '../landing-aboutus/landing-aboutus.component';
+<<<<<<< Updated upstream
 import { LandingEncuentraComponent } from '../landing-encuentra/landing-encuentra.component';
 import { LandingUneteComponent } from '../landing-unete/landing-unete.component';
 import { LandingComofuncionaComponent } from "../landing-comofunciona/landing-comofunciona.component";
+=======
+import { LandingComofuncionaComponent } from '../landing-comofunciona/landing-comofunciona.component';
+import { LandingEncuentraComponent } from '../landing-encuentra/landing-encuentra.component';
+
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-home',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [NavbarComponent, FooterComponent, RouterLink,LandingTestimonialComponent,LandingAboutusComponent, LandingEncuentraComponent,LandingComofuncionaComponent,LandingUneteComponent],
+=======
+  imports: [NavbarComponent, FooterComponent, RouterLink, LandingTestimonialComponent, LandingAboutusComponent, LandingEncuentraComponent, LandingComofuncionaComponent],
+>>>>>>> Stashed changes
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

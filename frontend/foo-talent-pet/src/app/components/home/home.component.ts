a24a6fbb97fffe81c11fc2a2e513122555135d31
Maicol bautista <pirlo1121s@gmail.com>
2024-07-05 13:25:57 -0500

    resolve merge in home.component.ts

--- frontend/foo-talent-pet/src/app/components/home/home.component.ts
+++ frontend/foo-talent-pet/src/app/components/home/home.component.ts
@@ -3,34 +3,27 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterLink } from "@angular/router";
 import { PetQuestService } from '../../service/pet-quest.service';
-import { OnInit } from '@angular/core';
+// import { OnInit } from '@angular/core';
 import { LandingTestimonialComponent } from "../landing-testimonial/landing-testimonial.component";
 import { LandingAboutusComponent } from '../landing-aboutus/landing-aboutus.component';
-<<<<<<< Updated upstream
 import { LandingEncuentraComponent } from '../landing-encuentra/landing-encuentra.component';
 import { LandingUneteComponent } from '../landing-unete/landing-unete.component';
 import { LandingComofuncionaComponent } from "../landing-comofunciona/landing-comofunciona.component";
-=======
-import { LandingComofuncionaComponent } from '../landing-comofunciona/landing-comofunciona.component';
-import { LandingEncuentraComponent } from '../landing-encuentra/landing-encuentra.component';
 
->>>>>>> Stashed changes
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-<<<<<<< Updated upstream
   imports: [NavbarComponent, FooterComponent, RouterLink,LandingTestimonialComponent,LandingAboutusComponent, LandingEncuentraComponent,LandingComofuncionaComponent,LandingUneteComponent],
-=======
-  imports: [NavbarComponent, FooterComponent, RouterLink, LandingTestimonialComponent, LandingAboutusComponent, LandingEncuentraComponent, LandingComofuncionaComponent],
->>>>>>> Stashed changes
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   data: any;
 
-  
+
   constructor(private petQuestService: PetQuestService) { }
 
 

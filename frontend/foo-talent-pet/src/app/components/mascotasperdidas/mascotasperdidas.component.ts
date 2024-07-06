35277b5cb8634a0c14607854b227a25eb13c63d3
Maicol bautista <pirlo1121s@gmail.com>
2024-07-05 22:38:47 -0500

    first part of the card

--- frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
+import { PetsCardComponent } from '../pets-card/pets-card.component';
 
 @Component({
   selector: 'app-mascotasperdidas',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent],
+  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent, PetsCardComponent],
   templateUrl: './mascotasperdidas.component.html',
   styleUrl: './mascotasperdidas.component.css'
 })

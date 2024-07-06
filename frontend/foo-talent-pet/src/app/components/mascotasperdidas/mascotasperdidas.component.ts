3a02b1c10d1f5b4880f81ebc286fe5d361eef25b
Maicol bautista <pirlo1121s@gmail.com>
2024-07-06 14:10:36 -0500

    card pets

--- frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
@@ -3,14 +3,20 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
 import { PetsCardComponent } from '../pets-card/pets-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mascotasperdidas',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent, PetsCardComponent],
+  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent, PetsCardComponent, CommonModule],
   templateUrl: './mascotasperdidas.component.html',
   styleUrl: './mascotasperdidas.component.css'
 })
 export class MascotasperdidasComponent {
+  pets = [
+    { name: 'Perro', age: 4 },
+    { name: 'Gato', age: 2 },
+    { name: 'Pájaro', age: 1 }
+  ];
 
 }
--- frontend/foo-talent-pet/src/app/components/pets-card/pets-card.component.ts
+++ frontend/foo-talent-pet/src/app/components/pets-card/pets-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pets-card',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './pets-card.component.css'
 })
 export class PetsCardComponent {
+  @Input() pet: any;
 
 }

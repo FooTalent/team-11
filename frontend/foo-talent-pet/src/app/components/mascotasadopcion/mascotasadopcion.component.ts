227e47ce1d02b2c6baab4b2e76c806ac3130a365
Maicol bautista <pirlo1121s@gmail.com>
2024-07-07 15:12:04 -0500

    title and text - mascotas perdidas

--- frontend/foo-talent-pet/src/app/components/mascotasadopcion/mascotasadopcion.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasadopcion/mascotasadopcion.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
+import { CommonModule } from '@angular/common';
+import { PetsCardComponent } from '../pets-card/pets-card.component';
 
 @Component({
   selector: 'app-mascotasadopcion',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent],
+  imports: [NavbarComponent, FooterComponent, FiltroperdidasComponent, CommonModule, PetsCardComponent],
   templateUrl: './mascotasadopcion.component.html',
   styleUrl: './mascotasadopcion.component.css'
 })
 export class MascotasadopcionComponent {
+  pets = [
+    { name: 'Perro', age: 4 },
+    { name: 'Gato', age: 2 },
+    { name: 'Pájaro', age: 1 },
+    { name: 'salmon', age: 1 },
+    { name: 'delfin', age: 1 }
+  ];
 
 }
--- frontend/foo-talent-pet/src/app/components/mascotasencontradas/mascotasencontradas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasencontradas/mascotasencontradas.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
+import { PetsCardComponent } from '../pets-card/pets-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mascotasencontradas',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent,FiltroperdidasComponent],
+  imports: [NavbarComponent, FooterComponent,FiltroperdidasComponent, PetsCardComponent, CommonModule],
   templateUrl: './mascotasencontradas.component.html',
   styleUrl: './mascotasencontradas.component.css'
 })
 export class MascotasencontradasComponent {
 
+  pets = [
+    { name: 'Perro', age: 4 },
+    { name: 'Gato', age: 2 },
+    { name: 'Pájaro', age: 1 },
+    { name: 'salmon', age: 1 },
+    { name: 'delfin', age: 1 }
+  ];
 }

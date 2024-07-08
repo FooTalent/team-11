4670892d7476bf70d3b149519e673c740d0540ca
Maicol bautista <pirlo1121s@gmail.com>
2024-07-08 18:02:27 -0500

    environments

--- frontend/foo-talent-pet/src/app/service/lostpets.service.ts
+++ frontend/foo-talent-pet/src/app/service/lostpets.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LostpetsService {
-  private apiUrl = 'https://notable-viva-adriansft-15c2b044.koyeb.app/api/posts/LOST';
+
+  // private apiUrl = 'https://notable-viva-adriansft-15c2b044.koyeb.app/api/posts/LOST';
 
   constructor(private http: HttpClient) { }
 
+  apiUrl: string = environment.apiUrl;
+
   getHealth(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl+"LOST");
   }
 
 }
---
+++ frontend/foo-talent-pet/src/environments/environments.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl : 'https://notable-viva-adriansft-15c2b044.koyeb.app/api/posts/'
+};

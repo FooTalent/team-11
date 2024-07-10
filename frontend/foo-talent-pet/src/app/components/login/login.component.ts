5cf39f50edb3e0c177af182f9120ec9a2a281587
Daniel Romero <romeroblessjunior@gmail.com>
2024-07-10 16:50:09 -0500

    test

--- frontend/foo-talent-pet/src/app/components/login/login.component.ts
+++ frontend/foo-talent-pet/src/app/components/login/login.component.ts
@@ -2,25 +2,43 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RegisterComponent } from '../register/register.component';
-// import { BtnLoginService } from '../../service/btn-login.service';
-// import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; 
+import { PetQuestService } from '../../service/pet-quest.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, RegisterComponent],
+  imports: [RouterLink, NavbarComponent, RegisterComponent,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   
-  // constructor(private btnLoginService: BtnLoginService) {}
+
+  constructor(private petQuestService: PetQuestService) {}
+  clickLogin(){
+    const userCredentials = {
+      email: "pirlo1121s@gmail.com",
+      password: "12345"
+    };
+    console.log('login');
+    this.petQuestService.login(userCredentials).subscribe((response: any) => {
+    console.log(response);
+   });
+  }
+
+oninit(){
   
+   
+   
+}
   // openModal(content:any){
   //   this.btnLoginService.openLogin(content);
   // }
 
   // close(){
   //   this.btnLoginService.closeLogin();
-  // }
+  // } 
 }
--- frontend/foo-talent-pet/src/app/service/pet-quest.service.ts
+++ frontend/foo-talent-pet/src/app/service/pet-quest.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PetQuestService {
-  private baseUrl = 'https://pet-quest.onrender.com/api/';
+  private baseUrl = 'https://notable-viva-adriansft-15c2b044.koyeb.app/api/auth/login';
 
   constructor(private http: HttpClient) { }
 
-  getEndpointData(endpoint: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}${endpoint}`);
+  login(payload:any): Observable<any> {
+	return this.http.post(this.baseUrl, payload);
   }
 }

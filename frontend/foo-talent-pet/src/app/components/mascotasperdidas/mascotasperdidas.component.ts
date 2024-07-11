12cd1d88906b1afbf8726180681210fd8752229e
Maicol bautista <pirlo1121s@gmail.com>
2024-07-11 17:36:19 -0500

    nueva API

--- frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
+++ frontend/foo-talent-pet/src/app/components/mascotasperdidas/mascotasperdidas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FiltroperdidasComponent } from '../filtroperdidas/filtroperdidas.component';
@@ -13,19 +13,30 @@ import { LostpetsService } from '../../service/lostpets.service';
   templateUrl: './mascotasperdidas.component.html',
   styleUrl: './mascotasperdidas.component.css'
 })
-export class MascotasperdidasComponent {
+export class MascotasperdidasComponent implements OnInit{
   // todo: crear interfaz de pets
+
   pets: any;
 
   constructor(private LostService: LostpetsService) {}
 
   ngOnInit() {
-    this.LostService.getHealth().subscribe(response => {
-      this.pets = response;
-      // console.log(this.pets);
-
-    }, error => {
-      console.error('Error fetching data', error);
+    this.LostService.getHealth().subscribe({
+      next: (response) => {
+        this.pets = response;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Observable completado');
+      },
     });
   }
+
+  // ngOnDestroy() {
+  //   if (this.subscription) {
+  //     this.subscription.unsubscribe();
+  //   }
+  // }
 }

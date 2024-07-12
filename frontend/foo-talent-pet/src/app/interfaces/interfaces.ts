966691927d9c7c65c6257fb5c2de74141e2f7456
Maicol bautista <pirlo1121s@gmail.com>
2024-07-12 11:23:20 -0500

    {+} new interfaces

---
+++ frontend/foo-talent-pet/src/app/interfaces/interfaces.ts
@@ -0,0 +1,43 @@
+export interface User {
+  id: string;
+  email: string;
+  name: string | null;
+  country: string | null;
+  province: string | null;
+  city: string | null;
+  phone: string | null;
+  profilePicture: string;
+}
+
+export interface Tag {
+  id: string;
+  name: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+}
+
+export interface Pet {
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  status: string;
+  speciesType: string;
+  gender: string;
+  province: string;
+  city: string;
+  locality: string;
+  contact: string;
+  createdAt: string;
+  user: User;
+  tags: Tag[];
+  colors: Color[];
+  images: string[];
+}
+
+export const pets: Pet[] = [
+  // Aquí irían los objetos que proporcionaste
+];

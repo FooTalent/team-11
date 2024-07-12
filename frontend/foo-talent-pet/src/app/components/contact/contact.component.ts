16afdd67edd02b37b81984c8b977119162734f0d
Maicol bautista <pirlo1121s@gmail.com>
2024-07-12 15:11:08 -0500

    {+} contact.services

--- frontend/foo-talent-pet/src/app/components/contact/contact.component.ts
+++ frontend/foo-talent-pet/src/app/components/contact/contact.component.ts
@@ -2,14 +2,36 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink } from '@angular/router';
+import { ContactService } from '../../service/contact.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent, RouterLink],
+  imports: [FooterComponent, NavbarComponent, RouterLink,FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  constructor(private contactService: ContactService) {}
 
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      this.contactService.sendContactForm(contactForm.value).subscribe({
+        next: (response) => {
+          console.log('Función correctamente', response);
+        },
+        error: (error) => {
+          console.error('Error al enviar el formulario', error);
+        },
+        complete: () => {
+          console.log('Envío del formulario completado');
+          // console.log(`info:  ${JSON.stringify(contactForm.value)}`);
+          contactForm.reset();
+
+
+        }
+      });
+    }
+  }
 }

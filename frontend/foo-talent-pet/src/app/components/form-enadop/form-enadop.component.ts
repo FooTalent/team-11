4878dc0a1af23b70428c859c97a86d3cbf8484dd
Juan Vacca <jvaccape@ibero.edu.co>
2024-07-15 19:04:20 -0500

    enrutamiento

--- frontend/foo-talent-pet/src/app/components/form-enadop/form-enadop.component.ts
+++ frontend/foo-talent-pet/src/app/components/form-enadop/form-enadop.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,inject} from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
+import { ConfirmacionService } from '../../service/confirmacion.service';
+
 
 @Component({
   selector: 'app-form-enadop',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './form-enadop.component.html',
   styleUrl: './form-enadop.component.css'
 })
 export class FormEnadopComponent {
+  private _confirmacionService = inject(ConfirmacionService);
+
+
+  public cargarConfirmacion():void{
+    this._confirmacionService.mostrarConfirmacion();
+  }
 
 }

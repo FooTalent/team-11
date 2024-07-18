9cb738341c782fb0ba2e49863469a408dba9e624
Daniel Romero <romeroblessjunior@gmail.com>
2024-07-18 14:00:40 -0500

    fix login

--- frontend/foo-talent-pet/src/app/service/pet-quest.service.ts
+++ frontend/foo-talent-pet/src/app/service/pet-quest.service.ts
@@ -9,15 +9,9 @@ import { Pet, PetResponse, PetWithComments } from '../interfaces/interfaces';
   providedIn: 'root'
 })
 export class PetQuestService {
-<<<<<<< Updated upstream
-  private baseUrl = environment.apiUrl+'auth/login';
-  private baseUrl1 = environment.apiUrl+'posts';
-  private apiUrl = environment.apiUrl;
-=======
   private baseUrl = environment.apiUrl;
   
 
->>>>>>> Stashed changes
 
 
   constructor(private http: HttpClient) { }
@@ -29,13 +23,9 @@ export class PetQuestService {
   }
 
 
-<<<<<<< Updated upstream
-  PostPet(payload:Pet,token:string) : Observable<PetResponse>{
-=======
   PostPet(payload:Pet,token:string){
     console.log(token);
     console.log(payload);
->>>>>>> Stashed changes
     const pet = {
         name: payload.name,
         description: payload.description,
@@ -59,17 +49,10 @@ export class PetQuestService {
     };
 
 
-<<<<<<< Updated upstream
-    return this.http.post<PetResponse>(this.baseUrl1, pet,options);
-  }
-
-  postImage(images:File [],id: string,token:string){
-=======
     return this.http.post(this.baseUrl+'posts', pet,options);
   }
 
   getPostUser(token:string){
->>>>>>> Stashed changes
     const headers = new HttpHeaders({
       Authorization: `Bearer ${token}`,
     });
@@ -77,19 +60,6 @@ export class PetQuestService {
     const options = {
       headers: headers,
     };
-<<<<<<< Updated upstream
-
-    const formData = new FormData();
-    images.forEach((image) => {
-      formData.append('images', image);
-    });
-
-    return this.http.post(`${this.baseUrl1}/${id}/images`, formData,options);
-  }
-
-  getPet(id:string): Observable<PetWithComments>{
-    return this.http.get<PetWithComments>(`${this.baseUrl1}/${id}/comments`);
-=======
     return this.http.get(this.baseUrl+'posts/user',options);
   }
 
@@ -102,6 +72,5 @@ export class PetQuestService {
       headers: headers,
     };
     return this.http.delete(this.baseUrl+'posts/'+id,options);
->>>>>>> Stashed changes
   }
 }
